6a8097e7305337697003c11133d493a70ff34a1b
Norman Yang <norman.yang@cbxsoftware.com>
2018-04-09 23:11:44 +0800

    Initial commit

---
+++ src/main/java/com/sample/security/SecuritySample/SecuritySampleApplication.java
@@ -0,0 +1,12 @@
+package com.sample.security.SecuritySample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuritySampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuritySampleApplication.class, args);
+	}
+}
